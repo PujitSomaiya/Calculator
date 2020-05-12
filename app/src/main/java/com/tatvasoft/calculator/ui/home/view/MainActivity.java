47d7a688686950467716ef7ccd6ff6961623b5e4
PujitSomaiya <pujit555@gmail.com>
2020-05-12 15:44:21 +0530

    -- ui change -- library added for expressions

--- app/src/main/java/com/tatvasoft/calculator/ui/home/view/MainActivity.java
+++ app/src/main/java/com/tatvasoft/calculator/ui/home/view/MainActivity.java
@@ -4,51 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.annotation.SuppressLint;
-import android.net.wifi.aware.SubscribeConfig;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.tatvasoft.calculator.R;
 import com.tatvasoft.calculator.databinding.ActivityMainBinding;
-import com.tatvasoft.calculator.util.CommonUtils;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
 import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
-    private static final char ADDITION = '+';
-    private static final char SUBTRACTION = '-';
-    private static final char MULTIPLICATION = '*';
-    private static final char DIVISION = '/';
-    private static final char REMAINDER = '%';
-    private char CURRENT_ACTION;
-    private double valueOne = Double.NaN;
-    private double valueTwo;
-    private DecimalFormat decimalFormat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        decimalFormat = new DecimalFormat("#.##########");
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initControls();
     }
 
     private void initControls() {
         initListeners();
+        setCalculation();
     }
 
+    private void setCalculation() {
+        binding.tvResult.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-    @SuppressLint("SetTextI18n")
-    private void edNumbers(String value) {
-        binding.edMain.setText(binding.edMain.getText() + value);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 
     private void initListeners() {
@@ -75,8 +77,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         binding.btnModule.setOnClickListener(this);
         binding.btnIn.setOnClickListener(this);
         binding.btnClear.setOnClickListener(this);
-        binding.edMain.requestFocus();
-
     }
 
     @SuppressLint("SetTextI18n")
@@ -85,115 +85,100 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         switch (id) {
             case R.id.btnZero:
-                edNumbers("0");
+                appendOnExpression("0",true);
                 break;
             case R.id.btnOne:
-                edNumbers("1");
+                appendOnExpression("1",true);
                 break;
             case R.id.btnTwo:
-                edNumbers("2");
+                appendOnExpression("2",true);
                 break;
             case R.id.btnThree:
-                edNumbers("3");
+                appendOnExpression("3",true);
                 break;
             case R.id.btnFour:
-                edNumbers("4");
+                appendOnExpression("4",true);
                 break;
             case R.id.btnFive:
-                edNumbers("5");
+                appendOnExpression("5",true);
                 break;
             case R.id.btnSix:
-                edNumbers("6");
+                appendOnExpression("6",true);
                 break;
             case R.id.btnSeven:
-                edNumbers("7");
+                appendOnExpression("7",true);
                 break;
             case R.id.btnEight:
-                edNumbers("8");
+                appendOnExpression("8",true);
                 break;
             case R.id.btnNine:
-                edNumbers("9");
+                appendOnExpression("9",true);
                 break;
             case R.id.btnPlus:
-                computeCalculation();
-                CURRENT_ACTION = ADDITION;
-                binding.tvMain.setText(decimalFormat.format(valueOne) + "+");
-                binding.edMain.setText(null);
+                appendOnExpression("+",true);
                 break;
             case R.id.btnMinus:
-                computeCalculation();
-                CURRENT_ACTION = SUBTRACTION;
-                binding.tvMain.setText(decimalFormat.format(valueOne) + "-");
-                binding.edMain.setText(null);
+                appendOnExpression("-",true);
                 break;
             case R.id.btnMul:
-                computeCalculation();
-                CURRENT_ACTION = MULTIPLICATION;
-                binding.tvMain.setText(decimalFormat.format(valueOne) + "*");
-                binding.edMain.setText(null);
+                appendOnExpression("*",true);
                 break;
             case R.id.btnDivide:
-                computeCalculation();
-                CURRENT_ACTION = DIVISION;
-                binding.tvMain.setText(decimalFormat.format(valueOne) + "/");
-                binding.edMain.setText(null);
+                appendOnExpression("/",true);
                 break;
             case R.id.btnModule:
-                computeCalculation();
-                CURRENT_ACTION = REMAINDER;
-                binding.tvMain.setText(decimalFormat.format(valueOne) + "%");
-                binding.edMain.setText(null);
+                appendOnExpression("%",true);
                 break;
             case R.id.btnEquals:
-                computeCalculation();
-                binding.tvMain.setText(decimalFormat.format(valueOne));
-                valueOne = Double.NaN;
-                CURRENT_ACTION = '0';
+                calculate();
+                break;
+            case R.id.btnIn:
+                appendOnExpression("(",true);
                 break;
             case R.id.btnClear:
-                valueOne = Double.NaN;
-                valueTwo = Double.NaN;
-                binding.edMain.setText("");
+                binding.tvResult.setText("");
                 binding.tvMain.setText("");
                 break;
             case R.id.btnDot:
-//                if (decimal) {
-//                    Toast.makeText(getApplicationContext(),"Add some values",Toast.LENGTH_SHORT).show();
-//                } else {
-//                    edMain.setText(edMain.getText() + ".");
-//                    decimal = true;
-//                }
+                binding.tvMain.setText(binding.tvMain.getText() + ".");
                 break;
             case R.id.btnSingleClear:
-                if (binding.edMain.getText().length() > 0) {
-                    CharSequence currentText = binding.edMain.getText();
-                    binding.edMain.setText(currentText.subSequence(0, currentText.length() - 1));
+                if (binding.tvResult.getText().length() > 0) {
+                    CharSequence currentText = binding.tvMain.getText();
+                    binding.tvMain.setText(currentText.subSequence(0, currentText.length() - 1));
                 }
                 break;
         }
     }
 
-    private void computeCalculation() {
-        if (!Double.isNaN(valueOne)) {
-            if (CommonUtils.isEmptyEditText(binding.edMain)){
-                Toast.makeText(getApplicationContext(),"Add value",Toast.LENGTH_SHORT).show();
-            }else {
-                valueTwo = Double.parseDouble(binding.edMain.getText().toString());
-                binding.edMain.setText(null);
-                if (CURRENT_ACTION == ADDITION)
-                    valueOne = this.valueOne + valueTwo;
-                else if (CURRENT_ACTION == SUBTRACTION)
-                    valueOne = this.valueOne - valueTwo;
-                else if (CURRENT_ACTION == MULTIPLICATION)
-                    valueOne = this.valueOne * valueTwo;
-                else if (CURRENT_ACTION == DIVISION)
-                    valueOne = this.valueOne / valueTwo;
-            }
+    private void calculate() {
+        try {
+            Expression expression = new ExpressionBuilder(binding.tvMain.getText().toString()).build();
+            Double result = expression.evaluate();
+//            Long longResult = ;
+//            if(result.equals(Double.valueOf(longResult)))
+//                binding.tvMain.setText(String.valueOf(longResult));
+//            else
+                binding.tvResult.setText(String.valueOf(result));
+
+        }catch (Exception e){
+            Log.d("Exception"," message : " + e.getMessage());
+        }
+    }
+
+
+    private void appendOnExpression(String string, Boolean  canClear) {
+
+        if(binding.tvResult.getText().length()>0){
+            binding.tvMain.setText("");
+        }
+        if (canClear) {
+            binding.tvResult.setText("");
+            binding.tvMain.append(string);
         } else {
-            try {
-                valueOne = Double.parseDouble(binding.edMain.getText().toString());
-            } catch (Exception e) {
-            }
+            binding.tvMain.append(binding.tvResult.getText().toString());
+            binding.tvMain.append(string);
+            binding.tvResult.setText("");
         }
     }
 }
